1d9e348cc7702fe74aed91d1c8052996f7c5afec
Cameron Johnson <61200181+CameronRJohnson@users.noreply.github.com>
2025-05-15 10:38:48 -0600

    Updated

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const PUBLIC_SUPABASE_URL = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
-const PUBLIC_ANON_KEY = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
+const PUBLIC_SUPABASE_URL = import.meta.env.VITE_PUBLIC_SUPABASE_URL || '';
+const PUBLIC_ANON_KEY = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY || '';
+
+if (!PUBLIC_SUPABASE_URL || !PUBLIC_ANON_KEY) {
+  console.error('Supabase environment variables are missing.');
+}
 
 export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_ANON_KEY);
