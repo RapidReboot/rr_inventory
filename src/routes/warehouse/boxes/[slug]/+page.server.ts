775d42150ec2a195b72755b1eb3a6cc20f8f3e95
Cameron Johnson <61200181+CameronRJohnson@users.noreply.github.com>
2025-05-15 16:40:39 -0600

    Added in prerender to boxes

--- src/routes/warehouse/boxes/[slug]/+page.server.ts
+++ src/routes/warehouse/boxes/[slug]/+page.server.ts
@@ -1,6 +1,5 @@
 import { supabase } from "$lib/supabase";
 import type { PageServerLoad } from './$types';
-export const prerender = false;
 
 export const load: PageServerLoad = async ({ params }) => {
   const { slug } = params;
@@ -16,3 +15,15 @@ export const load: PageServerLoad = async ({ params }) => {
 
   return { box };
 };
+
+import type { EntryGenerator } from './$types';
+
+export const entries: EntryGenerator = () => {
+	return [
+        { slug: 'Arm' },
+        { slug: 'Leg' },
+        { slug: 'Group Test' },
+	];
+};
+
+export const prerender = true;
