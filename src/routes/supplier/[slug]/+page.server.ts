67b5fb2c248a3558008a9639df1a6c58bb67048d
Cameron Johnson <cajoro935@gmail.com>
2025-06-03 00:47:53 -0600

    Update +page.server.ts

--- src/routes/supplier/[slug]/+page.server.ts
+++ src/routes/supplier/[slug]/+page.server.ts
@@ -1,20 +1,4 @@
 import { supabase } from "$lib/supabase";
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async ({ params }) => {
-  const { slug } = params;
-  const { data: supplier, error } = await supabase
-    .from("suppliers")
-    .select()
-    .eq('name', slug)
-    .single();
-
-  if (error) {
-    console.error("Supabase error:", error);
-  }
-
-  return { supplier };
-};
 
 import type { EntryGenerator } from './$types';
 
